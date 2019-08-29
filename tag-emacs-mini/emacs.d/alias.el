62a7a404b48af4c1cfe532871764a82c62f8d66e
Ludovic Kuzma <ludovic.kuzma@caldera.com>
2019-08-29 12:28:05 +0200

    emacs : Define some major mode aliases.

--- tag-emacs-mini/emacs.d/alias.el
+++ tag-emacs-mini/emacs.d/alias.el
@@ -1,5 +1,12 @@
-;; Alias definition file
 ;; ~/.emacs.d/alias.el
+;; Alias definition file
+;; Author : Kuzma Ludovic
 
 ;; Replace yes/no by y/n
 (defalias 'yes-or-no-p 'y-or-n-p)
+
+;; Some major modes aliases
+(defalias 'tm 'text-mode)
+(defalias 'hexm 'hexl-mode)
+(defalias 'cm 'c-mode)
+(defalias 'cpm 'c++-mode)
